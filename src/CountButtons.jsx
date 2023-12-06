fc5f8a04f41ea244f7e817e4fe34b819438335ff
aka <aaronkaltman@gmail.com>
2023-12-06 10:32:59 -0500

    adding counter +-1

--- src/CountButtons.jsx
+++ src/CountButtons.jsx
@@ -3,7 +3,10 @@ import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 const CountButtonsEl = ({ setCount }) => {
   return (
     <div className="button-container">
-      <button className="count-btn">
+      <button
+        onClick={() => setCount((prev) => prev - 1)}
+        className="count-btn"
+      >
         <MinusIcon className="count-btn-icon" />
       </button>
       <button
